915be7db763999ee58c713718ec357d632ca4c99
Natito9 <179492180+Natito9@users.noreply.github.com>
2024-10-10 10:04:09 +0200

    add book functions and diaplay books

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
+//library array , were books will be stored
+const myLibrary = [];
+
+//object constructor
 function Book(title, author,pages,read){
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read,
-    this.info = function(){console.log(`Book info! Title: ${this.title}, Author:${this.author}, Number of pages:${this.pages}, Read:${this.read}.`)}
+    this.read = read === "yes"; //boolean
+    this.info = function(){console.log(`Book info! Title: ${this.title}, Author:${this.author}, Number of pages:${this.pages}, Read:${this.read ? 'yes' : 'no'}.`)}
     
 }
 
-const theHobbit = new Book ("The hobbit","J. R. R. Tolkien","320","yes")
-console.log(theHobbit.info());
+//function to add book to array
+
+function addBook() {
+    const title = prompt("Enter the book title:");
+    const author = prompt("Enter the author's name:");
+    const pages = prompt("Enter the number of pages:");
+    const read = prompt("Have you read this book? (yes/no)");
+
+    const newBook = new Book (title, author, pages, read);
+    myLibrary.push(newBook);
+
+    console.log(`Book Added: ${newBook.info()}`);
+}
+
+//display books 
+
+function displayLibrary() {
+    console.log("My Library:");
+    myLibrary.forEach(book => {
+        console.log(book.info());
+    });
+}
+
+addBook(); // Prompt to add a book
+
+displayLibrary(); // Show all books in the library
